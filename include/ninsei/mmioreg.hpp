86337bf8fe9541eee86b8f042823660dc5cbe9b6
haitaim <76502924+haitaim@users.noreply.github.com>
2021-02-04 12:27:38 -0600

    Change namespace indentation

--- include/ninsei/mmioreg.hpp
+++ include/ninsei/mmioreg.hpp
@@ -9,45 +9,43 @@
 #include <type_traits>
 
 namespace ninsei {
+    namespace readWriteMod {
+        // Modifier tags
+        class Read_only {};
+        class Write_only {};
+        class Read_write : public Read_only, public Write_only {};
 
-namespace readWriteMod {
-    // Modifier tags
-    class Read_only {};
-    class Write_only {};
-    class Read_write : public Read_only, public Write_only {};
-
-    template <typename T>
-    concept ReadAccess = std::derived_from<T, Read_only>;
-    template <typename T>
-    concept WriteAccess = std::derived_from<T, Write_only>;
-    template <typename T>
-    concept RegisterAccess = ReadAccess<T> || WriteAccess<T>;
-}
-
-template <std::unsigned_integral Reg_size, readWriteMod::RegisterAccess Reg_access, std::uint32_t address>
-class Mem_mapped_reg {
-public:
-    Mem_mapped_reg() : reg_address { reinterpret_cast<volatile Reg_size*>(address) } {};
-
-    [[nodiscard("Unused volatile read")]]
-    Reg_size read() const noexcept requires readWriteMod::ReadAccess<Reg_access> {
-        return *reg_address;
+        template <typename T>
+        concept ReadAccess = std::derived_from<T, Read_only>;
+        template <typename T>
+        concept WriteAccess = std::derived_from<T, Write_only>;
+        template <typename T>
+        concept RegisterAccess = ReadAccess<T> || WriteAccess<T>;
     }
 
-    Reg_size& operator=(Reg_size bitmask) noexcept requires readWriteMod::WriteAccess<Reg_access> {
-        *reg_address = bitmask;
-        return *this;
-    }
+    template <std::unsigned_integral Reg_size, readWriteMod::RegisterAccess Reg_access, std::uint32_t address>
+    class Mem_mapped_reg {
+    public:
+        Mem_mapped_reg() : reg_address { reinterpret_cast<volatile Reg_size*>(address) } {};
 
-    Mem_mapped_reg(const Mem_mapped_reg&) = delete;
-    Mem_mapped_reg(Mem_mapped_reg&&) = delete;
-    Mem_mapped_reg& operator=(const Mem_mapped_reg&) = delete;
-    Mem_mapped_reg& operator=(Mem_mapped_reg&&) = delete;
+        [[nodiscard("Unused volatile read")]]
+        Reg_size read() const noexcept requires readWriteMod::ReadAccess<Reg_access> {
+            return *reg_address;
+        }
 
-private:
-    volatile Reg_size* reg_address;
-};
+        Reg_size& operator=(Reg_size bitmask) noexcept requires readWriteMod::WriteAccess<Reg_access> {
+            *reg_address = bitmask;
+            return *this;
+        }
 
+        Mem_mapped_reg(const Mem_mapped_reg&) = delete;
+        Mem_mapped_reg(Mem_mapped_reg&&) = delete;
+        Mem_mapped_reg& operator=(const Mem_mapped_reg&) = delete;
+        Mem_mapped_reg& operator=(Mem_mapped_reg&&) = delete;
+
+    private:
+        volatile Reg_size* reg_address;
+    };
 }
 
 #endif // NINSEI_MMIO_REG_HPP
--- include/ninsei/regalias.hpp
+++ include/ninsei/regalias.hpp
@@ -5,13 +5,11 @@
 #include <cstdint>
 
 namespace ninsei {
+    constexpr inline std::uint32_t mmio_address = 0x0400'0000;
 
-constexpr inline std::uint32_t mmio_address = 0x0400'0000;
-
-using Display_control = Mem_mapped_reg<std::uint32_t, readWriteMod::Read_write, mmio_address>;
-using Display_status = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, mmio_address + 0x004>;
-using Vertical_count = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_only, mmio_address + 0x006>;
-
+    using Display_control = Mem_mapped_reg<std::uint32_t, readWriteMod::Read_write, mmio_address>;
+    using Display_status = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, mmio_address + 0x004>;
+    using Vertical_count = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_only, mmio_address + 0x006>;
 }
 
 #endif // NINSEI_REG_ALIAS_HPP

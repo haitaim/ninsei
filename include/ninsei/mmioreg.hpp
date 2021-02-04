3a9f3e1e628deccd692710d393eecdfa7bd94fab
haitaim <76502924+haitaim@users.noreply.github.com>
2021-02-03 18:50:29 -0600

    Create MMIO register file

---
+++ include/ninsei/mmioreg.hpp
@@ -0,0 +1,11 @@
+// mmioreg.hpp
+//
+// Implements general register class which includes read/write protection.
+#ifndef NINSEI_MMIO_REG_HPP
+#define NINSEI_MMIO_REG_HPP
+
+class Mem_mapped_reg {
+
+};
+
+#endif // NINSEI_MMIO_REG_HPP

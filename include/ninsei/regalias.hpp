68eff7be88edee67775314515a5b80ef3406cc72
haitaim <76502924+haitaim@users.noreply.github.com>
2021-02-03 22:27:42 -0600

    Start register alias definitions

---
+++ include/ninsei/regalias.hpp
@@ -0,0 +1,17 @@
+#ifndef NINSEI_REG_ALIAS_HPP
+#define NINSEI_REG_ALIAS_HPP
+
+#include "mmioreg.hpp"
+#include <cstdint>
+
+namespace ninsei {
+
+constexpr inline std::uint32_t mmio_address = 0x0400'0000;
+
+using Display_control = Mem_mapped_reg<std::uint32_t, readWriteMod::Read_write, mmio_address>;
+using Display_status = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, mmio_address + 0x004>;
+using Vertical_count = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_only, mmio_address + 0x006>;
+
+}
+
+#endif // NINSEI_REG_ALIAS_HPP

d5f805ec76640da47614cf8f2505270932dd5964
haitaim <76502924+haitaim@users.noreply.github.com>
2021-02-04 21:03:27 -0600

    Add memory constants

---
+++ include/ninsei/memmap.hpp
@@ -0,0 +1,19 @@
+#ifndef NINSEI_MEM_MAP_HPP
+#define NINSEI_MEM_MAP_HPP
+
+#include <cstdint>
+
+namespace ninsei {
+    namespace memAddress {
+        inline constexpr std::uint32_t external_work_ram = 0x0200'0000;
+        inline constexpr std::uint32_t internal_work_ram = 0x0300'0000;
+        inline constexpr std::uint32_t io_registers = 0x0400'0000;
+        inline constexpr std::uint32_t pallete_ram = 0x0500'0000;
+        inline constexpr std::uint32_t video_ram = 0x0600'0000;
+        inline constexpr std::uint32_t object_attribute = 0x0700'0000;
+        inline constexpr std::uint32_t cartridge_rom = 0x0800'0000;
+        inline constexpr std::uint32_t cartridge_sram = 0x0E00'0000;
+    }
+}
+
+#endif // NINSEI_MEM_MAP_HPP

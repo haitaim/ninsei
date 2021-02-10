6f8cde1914fb38dc376e731c8ca770ad0d87127a
haitaim <76502924+haitaim@users.noreply.github.com>
2021-02-09 23:23:24 -0600

    Add display control register mask

---
+++ include/ninsei/regmask.hpp
@@ -0,0 +1,54 @@
+#ifndef NINSEI_REG_MASK_HPP
+#define NINSEI_REG_MASK_HPP
+
+#include "mmioreg.hpp"
+#include <cstdint>
+
+namespace ninsei::reg {
+namespace mask {
+    template<typename Reg_size>
+    Reg_size enable_bit(Reg_size bitmask, const unsigned int bit_position, bool enable) {
+        if (enable) {
+            bitmask |= (1 << bit_position);
+        } else {
+            bitmask &= ~(1 << bit_position);
+        }
+        return bitmask;
+    }
+    
+    class Display_control : public Register_mask {
+    public:
+        consteval Display_control() : bitmask { 0 } {}
+        
+        template <unsigned mode> requires (mode >= 0 && mode <= 3)
+        void set_mode() {
+            bitmask &= ~(3);
+            bitmask |= mode;
+        }
+        void swap_page() {
+            bitmask ^= (1 << 4);
+        }
+        void oam_hblank_access(bool enable) {
+            bitmask = enable_bit(bitmask, 5, enable);
+        }
+        void force_blank(bool enable) {
+            bitmask = enable_bit(bitmask, 7, enable);
+        }
+        template <unsigned bg_num> requires (bg_num >= 0 && bg_num <= 3)
+        void enable_background(bool enable) {
+            bitmask = enable_bit(bitmask, 8 + bg_num, enable);
+        }
+        void enable_obj_background(bool enable) {
+            bitmask = enable_bit(bitmask, 0xC, enable);
+        }
+        std::uint32_t raw_bitmask() {
+            return bitmask;
+        }
+
+    private:
+        std::uint32_t bitmask;
+    };
+}
+}
+
+#endif // NINSEI_REG_MASK_HPP

57951cc904082eca72f793539b00b889a16403c8
haitaim <76502924+haitaim@users.noreply.github.com>
2021-02-11 17:27:36 -0600

    Template and format register aliases

--- include/ninsei/regalias.hpp
+++ include/ninsei/regalias.hpp
@@ -20,70 +20,159 @@ namespace lcd {
         memAddress::io_registers + 0x0004,
         mask::Display_status
         >;
-    using Vertical_count = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_only, memAddress::io_registers + 0x0006>;
+    using Vertical_count = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_only,
+        memAddress::io_registers + 0x0006
+        >;
 
-    using Background0_control = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0008>;
-    using Background1_control = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x000A>;
-    using Background2_control = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x000C>;
-    using Background3_control = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x000E>;
+    template <unsigned bg_num> requires (bg_num >= 0 && bg_num <= 3)
+    using Background_control = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0008 + (2 * bg_num)
+        >;
+    template <unsigned bg_num> requires (bg_num >= 0 && bg_num <= 3)
+    using Background_horizontal_offset = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Write_only,
+        memAddress::io_registers + 0x0010 + (4 * bg_num)
+        >;
+    template <unsigned bg_num> requires (bg_num >= 0 && bg_num <= 3)
+    using Background_vertical_offset = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Write_only,
+        memAddress::io_registers + 0x0012 + (4 * bg_num)
+        >;
+    template <unsigned bg_num, char parameter>
+    requires ((bg_num == 2 || bg_num == 3) && (parameter >= 'A' && parameter <= 'D'))
+    using Background_rotation_scaling = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Write_only,
+        memAddress::io_registers + 0x0020 + ((bg_num - 2) * 0x10) + ((parameter - 'A') * 2)
+        >;
+    template <unsigned bg_num, char variable>
+    requires ((bg_num == 2 || bg_num == 3) && (variable == 'x' || variable == 'y'))
+    using Background_reference_point = Mem_mapped_reg<
+        std::uint32_t,
+        readWriteMod::Write_only,
+        memAddress::io_registers + 0x0028  + ((bg_num - 2) * 0x10) + ((variable - 'x') * 4)
+        >;
 
-    using Background0_horizontal_offset = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0010>;
-    using Background0_vertical_offset = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0012>;
-    using Background1_horizontal_offset = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0014>;
-    using Background1_vertical_offset = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0016>;
-    using Background2_horizontal_offset = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0018>;
-    using Background2_vertical_offset = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x001A>;
-    using Background3_horizontal_offset = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x001C>;
-    using Background3_vertical_offset = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x001E>;
+    template <unsigned window_num> requires (window_num == 0 || window_num == 1)
+    using Window_horizontal_dimension = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Write_only,
+        memAddress::io_registers + 0x0040 + (window_num * 2)
+        >;
+    template <unsigned window_num> requires (window_num == 0 || window_num == 1)
+    using Window_vertical_dimension = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Write_only,
+        memAddress::io_registers + 0x0044 + (window_num * 2)
+        >;
 
-    using Background2_parameter_a = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0020>;
-    using Background2_parameter_b = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0022>;
-    using Background2_parameter_c = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0024>;
-    using Background2_parameter_d = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0026>;
-
-    using Background2_x_reference = Mem_mapped_reg<std::uint32_t, readWriteMod::Write_only, memAddress::io_registers + 0x0028>;
-    using Background2_y_reference = Mem_mapped_reg<std::uint32_t, readWriteMod::Write_only, memAddress::io_registers + 0x002C>;
-
-    using Background3_parameter_a = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0030>;
-    using Background3_parameter_b = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0032>;
-    using Background3_parameter_c = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0034>;
-    using Background3_parameter_d = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0036>;
-
-    using Background3_x_reference = Mem_mapped_reg<std::uint32_t, readWriteMod::Write_only, memAddress::io_registers + 0x0038>;
-    using Background3_y_reference = Mem_mapped_reg<std::uint32_t, readWriteMod::Write_only, memAddress::io_registers + 0x003C>;
-
-    using Window0_horizontal_dimension = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0040>;
-    using Window1_horizontal_dimension = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0042>;
-    using Window0_vertical_dimension = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0044>;
-    using Window1_vertical_dimension = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0046>;
-
-    using Inside_windows = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0048>;
-    using Outside_windows = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x004A>;
-
-    using Mosaic_size = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x004C>;
-
-    using Colour_effects = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0050>;
-    using Alpha_blend = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0052>;
-    using Brightness = Mem_mapped_reg<std::uint16_t, readWriteMod::Write_only, memAddress::io_registers + 0x0054>;
+    using Inside_windows = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0048
+        >;
+    using Outside_windows = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x004A
+        >;
+    using Mosaic_size = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Write_only,
+        memAddress::io_registers + 0x004C
+        >;
+    using Colour_effects = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0050
+        >;
+    using Alpha_blend = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0052
+        >;
+    using Brightness = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Write_only,
+        memAddress::io_registers + 0x0054
+        >;
 }
 namespace sound {
-    using Channel1_sweep = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0060>;
-    using Channel1_duty = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0062>;
-    using Channel1_frequency = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0064>;
+    using Channel1_sweep = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0060
+        >;
+    using Channel1_duty = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0062
+        >;
+    using Channel1_frequency = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0064
+        >;
 
-    using Channel2_duty = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0068>;
-    using Channel2_frequency = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x006C>;
+    using Channel2_duty = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0068
+        >;
+    using Channel2_frequency = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x006C
+        >;
 
-    using Channel3_stop_wave = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0070>;
-    using Channel3_length = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0072>;
-    using Channel3_frequency = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0074>;
+    using Channel3_stop_wave = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0070
+        >;
+    using Channel3_length = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0072
+        >;
+    using Channel3_frequency = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0074
+        >;
 
-    using Channel4_length = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0078>;
-    using Channel4_frequency = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x007C>;
+    using Channel4_length = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0078
+        >;
+    using Channel4_frequency = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x007C
+        >;
 
-    using Stereo_volume = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0080>;
-    using Mixing_dma = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0082>;
-    using Sound_on_off = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0084>;
+    using Stereo_volume = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0080
+        >;
+    using Mixing_dma = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0082
+        >;
+    using Sound_on_off = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0084
+        >;
 }
 namespace dma {
 
@@ -95,8 +184,16 @@ namespace serialCom {
 
 }
 namespace keypad {
-    using Status = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_only, memAddress::io_registers + 0x0130>;
-    using Interrupt_control = Mem_mapped_reg<std::uint16_t, readWriteMod::Read_write, memAddress::io_registers + 0x0132>;
+    using Status = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_only,
+        memAddress::io_registers + 0x0130
+        >;
+    using Interrupt_control = Mem_mapped_reg<
+        std::uint16_t,
+        readWriteMod::Read_write,
+        memAddress::io_registers + 0x0132
+        >;
 }
 namespace interrupt {
 

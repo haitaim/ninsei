f2d693d32a2373fe35084ce7502eb9e5d4076bd4
haitaim <76502924+haitaim@users.noreply.github.com>
2021-02-28 17:10:31 -0600

    Fix spelling mistake

--- include/ninsei/memmap.hpp
+++ include/ninsei/memmap.hpp
@@ -10,7 +10,7 @@ namespace ninsei::memAddress {
     inline constexpr std::uint32_t external_work_ram = 0x0200'0000;
     inline constexpr std::uint32_t internal_work_ram = 0x0300'0000;
     inline constexpr std::uint32_t io_registers = 0x0400'0000;
-    inline constexpr std::uint32_t pallete_ram = 0x0500'0000;
+    inline constexpr std::uint32_t palette_ram = 0x0500'0000;
     inline constexpr std::uint32_t video_ram = 0x0600'0000;
     inline constexpr std::uint32_t object_attribute = 0x0700'0000;
     inline constexpr std::uint32_t cartridge_rom = 0x0800'0000;

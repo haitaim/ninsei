555d4152ef2c3264ea182d4e4dad7d3cd15c4ff8
haitaim <76502924+haitaim@users.noreply.github.com>
2021-03-04 17:52:07 -0600

    Add bitmap frame selection

--- include/ninsei/bitmap.hpp
+++ include/ninsei/bitmap.hpp
@@ -6,6 +6,10 @@
 #include <cstdint>
 
 namespace ninsei::bitmap {
+    constexpr std::uint32_t frame_offset(std::uint32_t frame_number) {
+        return (frame_number & 1) * 0xA000;
+    }
+
 namespace mode3 {
     inline void plot(std::uint32_t x, std::uint32_t y, Colour15 colour) {
         reinterpret_cast<volatile Colour15*>(memAddress::video_ram)[y * video::lcd::width + x] = colour;
@@ -31,10 +35,10 @@ namespace mode3 {
     }
 }
 namespace mode4 {
-    inline void fill(std::uint32_t palette_num) {
+    inline void fill(std::uint32_t palette_num, std::uint32_t frame_number = 0) {
         std::uint32_t word_length_palettes = palette_num | (palette_num << 8) | (palette_num << 16) | (palette_num << 24);
         for (std::uint32_t i = 0; i < ((video::lcd::width * video::lcd::height) >> 1); ++i) {
-            reinterpret_cast<volatile std::uint32_t*>(memAddress::video_ram)[i] = word_length_palettes;
+            reinterpret_cast<volatile std::uint32_t*>(memAddress::video_ram + frame_offset(frame_number))[i] = word_length_palettes;
         }
     }
 }
@@ -44,14 +48,14 @@ namespace mode5 {
         inline constexpr std::uint32_t height = 128;
     }
 
-    inline void plot(std::uint32_t x, std::uint32_t y, Colour15 colour) {
-        reinterpret_cast<volatile Colour15*>(memAddress::video_ram)[y * lcd::width + x] = colour;
+    inline void plot(std::uint32_t x, std::uint32_t y, Colour15 colour, std::uint32_t frame_number = 0) {
+        reinterpret_cast<volatile Colour15*>(memAddress::video_ram + frame_offset(frame_number))[y * lcd::width + x] = colour;
     }
 
-    inline void rectangle(std::uint32_t left_x, std::uint32_t top_y, std::uint32_t right_x, std::uint32_t bottom_y, Colour15 colour) {
+    inline void rectangle(std::uint32_t left_x, std::uint32_t top_y, std::uint32_t right_x, std::uint32_t bottom_y, Colour15 colour, std::uint32_t frame_number = 0) {
         std::uint32_t length = right_x - left_x;
         std::uint32_t height = bottom_y - top_y;
-        auto upper_left = reinterpret_cast<volatile Colour15*>(memAddress::video_ram + (((top_y * mode5::lcd::width) + left_x) * 2));
+        auto upper_left = reinterpret_cast<volatile Colour15*>(memAddress::video_ram + frame_offset(frame_number) + (((top_y * mode5::lcd::width) + left_x) * 2));
 
         for (std::uint32_t i = 0; i < height; ++i) {
             for (std::uint32_t j = 0; j < length; ++j) {
@@ -60,10 +64,10 @@ namespace mode5 {
         }
     }
 
-    inline void fill(Colour15 colour) {
+    inline void fill(Colour15 colour, std::uint32_t frame_number = 0) {
         std::uint32_t word_length_colours = colour | (colour << 16);
         for (std::uint32_t i = 0; i < ((mode5::lcd::width * mode5::lcd::height) >> 1); ++i) {
-            reinterpret_cast<volatile std::uint32_t*>(memAddress::video_ram)[i] = word_length_colours;
+            reinterpret_cast<volatile std::uint32_t*>(memAddress::video_ram + frame_offset(frame_number))[i] = word_length_colours;
         }
     }
 }
